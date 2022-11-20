b8d4ca5312db4178aae64cb5157aa3c08d33fcf8
ben <bjelinek42@gmail.com>
2022-11-20 14:29:34 -0700

    user serializer shows user groups bets

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,15 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email
+  attributes :id, :name, :email, :bets
   has_many :user_groups
+
+  def bets
+    user_groups = UserGroup.where(user_id: current_user.id)
+    bets = []
+    user_groups.each do |ug|
+      bets << Bet.where(group_id: ug.group_id)
+    end
+    return bets
+  end
   
 
 end
